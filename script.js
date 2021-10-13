ca7ea09f4c5ae81f3b60326939d6771028b1ca18
xafixav <xafiworld@gmail.com>
2021-10-13 01:44:27 -0300

    toDoList

---
+++ script.js
@@ -0,0 +1,94 @@
+const getListaTarefas = document.getElementById('lista-tarefas');
+const getInput = document.getElementById('texto-tarefa');
+const getButton = document.getElementById('criar-tarefa');
+const getButtonEraseAll = document.getElementById('apaga-tudo');
+const getButtonErase = document.getElementById('remover-finalizados');
+const getSaveTaskList = document.getElementById('salvar-tarefas');
+
+let listArray;
+if (localStorage.getItem('lista') === null) {
+  listArray = [];
+} else {
+  listArray = localStorage.getItem('lista').split(',');
+}
+
+function avoidDoubleItems(compara) {
+  const ulList = document.getElementsByTagName('li');
+  for (let check = 0; check < ulList.length; check += 1) {
+    if (ulList[check].innerText === compara) {
+      return true;
+    }
+  }
+}
+
+function addOrRemoveClass(event) {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
+}
+
+function removeCompleted() {
+  const completedArray = [];
+  const markedList = document.querySelectorAll('.completed');
+  markedList.forEach((element) => {
+    completedArray.push(element.innerText);
+    element.remove();
+  });
+  const storageList = localStorage.getItem('lista').split(',');
+  listArray = storageList.filter((value) => !completedArray.includes(value));
+}
+
+function eraseList() {
+  const getTarefas = getListaTarefas.childNodes;
+  while (getTarefas.length > 0) {
+    getTarefas.forEach((element) => element.remove());
+  }
+  localStorage.removeItem('lista');
+}
+
+function createItem(value) {
+  const li = document.createElement('li');
+  li.innerText = value;
+  li.addEventListener('click', addOrRemoveClass, false);
+  li.addEventListener('dblclick', addOrRemoveClass, false);
+  document.querySelector('ol').appendChild(li);
+}
+
+function addList() {
+  const getTarefasValue = getInput.value;
+  if (avoidDoubleItems(getTarefasValue) === true) {
+    alert('List cannot have double items');
+  } else {
+    createItem(getTarefasValue);
+    listArray.push(getTarefasValue);    
+  }
+  getInput.value = '';
+}
+
+function addOnLoad() {
+  const getStorage = localStorage.getItem('lista').split(',');
+  getStorage.forEach((element) => {
+    createItem(element);
+  });
+}
+
+function saveList() {
+  if (localStorage.getItem('lista') !== undefined) {
+    localStorage.setItem('lista', listArray);
+  } else {
+    localStorage.removeItem('lista');
+  }
+}
+
+getButton.addEventListener('click', addList);
+getButtonEraseAll.addEventListener('click', eraseList);
+getButtonErase.addEventListener('click', removeCompleted);
+getSaveTaskList.addEventListener('click', saveList);
+
+window.onload = () => {
+  if (localStorage.getItem('lista') !== null) {
+    addOnLoad();
+  }
+};
